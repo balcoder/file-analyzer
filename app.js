7c7bc98ec2da9f4b26a7654d0ece47c6cb121874
Des Barrett <des.barrett@gmail.com>
2018-10-19 22:35:52 +0100

    added some comments

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// add our routes
 app.use('/', indexRouter);
 app.use('/api/fileanalyse', fileRouter);
 
--- routes/file.js
+++ routes/file.js
@@ -8,6 +8,8 @@ var multer  = require('multer')
 // set the destination to the uploads folder in root
 var upload = multer({ dest: 'uploads/' })
 
+// Multer adds body,file or files object to the request object,body will hold
+// text fields if any, use upload.single for one file, upload.array for multiple
 router.post('/', upload.single('upfile'), function(req, res, next) {
   res.json({'name': req.file.originalname, 'type': req.file.mimetype, 'size': req.file.size})
 });
